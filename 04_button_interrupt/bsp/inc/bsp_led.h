64de723784401fa49c34e36f3940ea7b835c3a11
YeSeungWan <tmddhks68@gmail.com>
2026-06-27 15:53:42 +0900

    feat: 04_button_interrupt 프로젝트 추가

---
+++ 04_button_interrupt/bsp/inc/bsp_led.h
@@ -0,0 +1,19 @@
+#ifndef BSP_LED_H_
+#define BSP_LED_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// ------------------------------//
+// * Extern Function             //
+// ------------------------------//
+void bsp_led_toggle_all(void);
+void bsp_led_toggle(uint8_t);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // BSP_LED_H_
